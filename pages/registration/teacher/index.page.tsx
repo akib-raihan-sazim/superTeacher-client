b05ab4be383155298ff3d018754a2fab4028791f
Akib Raihan <akib.raihan@sazim.io>
2024-08-21 23:50:12 +0600

    feat(ST-71): add teacher registration form and also form validation

---
+++ pages/registration/teacher/index.page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import TeacherRegistrationContainer from "@/modules/Registration/containers/teacher/TeacherRegistrationContainer";
+
+const TeacherRegistration = () => <TeacherRegistrationContainer />;
+
+export default TeacherRegistration;
