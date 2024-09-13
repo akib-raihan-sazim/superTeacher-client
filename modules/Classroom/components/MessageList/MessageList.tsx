e922735f975197e05bfc9b9551c65e5e54e2a401
Akib Raihan <akib.raihan@sazim.io>
2024-09-13 18:59:09 +0600

    feat(ST-127): integrate socket for realtime messaging

--- modules/Classroom/components/MessageList/MessageList.tsx
+++ modules/Classroom/components/MessageList/MessageList.tsx
@@ -1,15 +1,50 @@
-import { Box, Text, LoadingOverlay } from "@mantine/core";
-import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 
+import { Box, Text, LoadingOverlay } from "@mantine/core";
+import { io, Socket } from "socket.io-client";
+
+import { WS_BASE_URL } from "@/shared/constants/env.constants";
+import { useAppSelector } from "@/shared/redux/hooks";
+import { selectAuthenticatedUser } from "@/shared/redux/reducers/user.reducer";
 import { useGetClassroomMessagesQuery } from "@/shared/redux/rtk-apis/messages/messages.api";
+import { IMessage } from "@/shared/redux/rtk-apis/messages/messages.interface";
 
 import MessageCard from "../MessageCard/MessageCard";
 import { IMessageListProps } from "./MessageList.interface";
 import useStyles from "./MessageList.styles";
 
 const MessageList: React.FC<IMessageListProps> = ({ classroomId }) => {
-  const { data: messages, isLoading } = useGetClassroomMessagesQuery(classroomId);
+  const {
+    data: initialMessages,
+    isLoading,
+    isSuccess,
+  } = useGetClassroomMessagesQuery(classroomId, {
+    skip: !classroomId,
+    refetchOnMountOrArgChange: true,
+  });
   const { classes } = useStyles();
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const user = useAppSelector(selectAuthenticatedUser);
+
+  useEffect(() => {
+    if (isSuccess && initialMessages) {
+      setMessages(initialMessages);
+    }
+  }, [initialMessages, isSuccess]);
+
+  useEffect(() => {
+    const socket: Socket = io(WS_BASE_URL);
+
+    socket.emit("joinClassroom", { classroomId, userId: user.userId });
+
+    socket.on("receiveMessage", (newMessage) => {
+      setMessages((prevMessages) => [newMessage, ...prevMessages]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [classroomId, user.userId]);
 
   if (!messages || messages.length === 0) {
     return (
@@ -20,7 +55,7 @@ const MessageList: React.FC<IMessageListProps> = ({ classroomId }) => {
   }
 
   const sortedMessages = [...messages].sort(
-    (a, b) => dayjs(b.createdAt).unix() - dayjs(a.createdAt).unix(),
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
   );
 
   return (
