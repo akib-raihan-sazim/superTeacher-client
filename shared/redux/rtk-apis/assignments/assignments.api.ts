1008a14a75c093e089a8f33367ebbf91be8c3d67
Akib Raihan <akib.raihan@sazim.io>
2024-09-23 17:05:15 +0600

    feat(ST-144): integrate edit assignment api

--- shared/redux/rtk-apis/assignments/assignments.api.ts
+++ shared/redux/rtk-apis/assignments/assignments.api.ts
@@ -30,8 +30,32 @@ const assignmentsApi = projectApi.injectEndpoints({
       }),
       providesTags: (_, __, classroomId) => [{ type: "Assignments", id: classroomId }],
     }),
+
+    updateAssignment: builder.mutation<
+      AssignmentsResponseDto,
+      { classroomId: number; assignmentId: number; data: CreateAssignmentDto }
+    >({
+      query: ({ classroomId, assignmentId, data }) => {
+        const formData = new FormData();
+        formData.append("file", data.file);
+        formData.append("title", data.title);
+        formData.append("description", data.description);
+        formData.append("dueDate", data.dueDate.toISOString());
+
+        return {
+          url: `/classrooms/${classroomId}/assignments/${assignmentId}`,
+          method: "PUT",
+          body: formData,
+        };
+      },
+      invalidatesTags: (_, __, { classroomId }) => [{ type: "Assignments", id: classroomId }],
+    }),
   }),
   overrideExisting: false,
 });
 
-export const { useCreateAssignmentMutation, useGetClassroomAssignmentsQuery } = assignmentsApi;
+export const {
+  useCreateAssignmentMutation,
+  useGetClassroomAssignmentsQuery,
+  useUpdateAssignmentMutation,
+} = assignmentsApi;
